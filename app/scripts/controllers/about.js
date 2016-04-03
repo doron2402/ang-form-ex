5bd4aa339f454e0a8a0a589e936e1d23e0127c52
Doron Segal <doron2402@gmail.com>
2016-04-03 13:16:37 -0700

    start

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angFormApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angFormApp
+ */
+angular.module('angFormApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angFormApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the angFormApp
+ */
+angular.module('angFormApp')
+  .controller('ContactCtrl', function () {
+    this.msg = 'Hi, Please feel free to contact us';
+  });
